a25a52fd094645e25a96b9312661f741d1024971
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-06-19 23:37:24 +0300

    del-superfluous

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
-
-
 const gallery = document.querySelector('.gallery');
-// console.log(gallery);
 
-const li = galleryItems.map(({ preview, original, description }) => {
+const createMarcup = galleryItems.map(({ preview, original, description }) => {
   return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -19,37 +15,29 @@ const li = galleryItems.map(({ preview, original, description }) => {
   </a>
 </li>`
 }).join("");
-// console.log(li);
-gallery.insertAdjacentHTML("beforeend", li);
+gallery.insertAdjacentHTML("beforeend", createMarcup);
+
 
 
 gallery.addEventListener("click", onShowImg);
 
+
 function onShowImg(event) {
   const { target } = event;
-  // console.log(mainPhoto)
+  
   if (!target.classList.contains('js-gallery__image')) {
     return;
   }
-  // console.log(mainPhoto)
-
+ 
   const origiImg = target.closest('.js-gallery__image').dataset.source;
-  const currentItem = galleryItems.find((source) =>  source === origiImg);
-  // console.log(origiImg)
-
-
+  // const currentItem = galleryItems.find((source) =>  source === origiImg);
+  
   const instance = basicLightbox.create(`<div class="modal">
     <img  width="800" height="600" src="${origiImg}"
     />
-    </div>
-
-  
+    </div>  
 `).show();
-  // instance.show();
-// console.log(instance)
-
 }
 
 
 
-// instance.show()
