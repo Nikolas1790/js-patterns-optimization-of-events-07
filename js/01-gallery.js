ba33aeb0d805dae8464bd58974828eddfad3a340
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-06-19 15:06:45 +0300

    photo

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,4 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-  =
+
+
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+const li = galleryItems.map(({ preview, original, description }) => {
+  return `
+      <li class="js-item item">
+        <img class="js-photo photo" src="${preview}" data-src="${original}" alt="${description}"/>
+      </li>`
+}).join("");
+console.log(li);
+
+gallery.insertAdjacentHTML("beforeend", li);
+
+// gallery.addEventListener("click", onShowImg);
+
+// function onShowImg(event) {
+//   const mainPhoto = event.target
+// }
